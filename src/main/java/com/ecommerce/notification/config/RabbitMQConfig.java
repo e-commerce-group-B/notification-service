3ebcb4042182ab25825b5d48ea65c2e8838e6f3b
Shashikala-manohari <shashi99ugc@gmail.com>
2026-06-28 15:05:02 +0530

    added RabbitMQ configuration

---
+++ src/main/java/com/ecommerce/notification/config/RabbitMQConfig.java
@@ -0,0 +1,29 @@
+package com.ecommerce.notification.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue:order-queue}")
+    private String queue;
+
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(mapper);
+    }
+}
